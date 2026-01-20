539eb09a5e1947cb3d7caa36e9270ff24771da9d
Andras <kaltenandras@gmail.com>
2026-01-20 16:22:49 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ Adafruit_BMP280 bme; // I2C
 MPU9250_asukiaaa mySensor;
 float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
 
+float minX, maxX, minY, maxY, minZ, maxZ;
+
 void setup() {
   Serial.begin(9600);
 
@@ -26,8 +28,20 @@ void setup() {
     Serial.println("BMP280 not found! Check wiring.");
     while (1);
   }
+
+  Serial.println("gX,gY,gZ");
+
+  minX = 1000;
+  minY = 1000;
+  minZ = 1000;
+  maxX = -1000;
+  maxY = -1000;
+  maxZ = -1000;
+
 }
 
+int amaount;
+bool canLoop = true;
 
 float oldGX, oldGY, oldGZ;
 
@@ -40,33 +54,66 @@ void loop() {
    
   //}
   
-  if (mySensor.gyroUpdate() == 0) {
-    gX = mySensor.gyroX();
-    gY = mySensor.gyroY();
-    gZ = mySensor.gyroZ();
-    //Serial.println("\tgyroX: " + String(gX));
-    //Serial.println("\tgyroY: " + String(gY));
+  if (amaount <= 1000) {
+    if (mySensor.gyroUpdate() == 0) {
+      gX = mySensor.gyroX();
+      gY = mySensor.gyroY();
+      gZ = mySensor.gyroZ();
+      //Serial.println("\tgyroX: " + String(gX));
+      //Serial.println("\tgyroY: " + String(gY));
+      
     
-  
+      /*
+      Serial.println("\tgX: " + String(gX));
+      Serial.println("\tgY: " + String(gY));
+      Serial.println("\tgZ: " + String(gZ));
 
-    Serial.println("\tgX: " + String(gX));
-    Serial.println("\tgY: " + String(gY));
-    Serial.println("\tgZ: " + String(gZ));
+      Serial.println("\told gX: " + String(oldGX));
+      Serial.println("\told gY: " + String(oldGY));
+      Serial.println("\told gZ: " + String(oldGZ));
 
-    Serial.println("\told gX: " + String(oldGX));
-    Serial.println("\told gY: " + String(oldGY));
-    Serial.println("\told gZ: " + String(oldGZ));
-
-    Serial.println("\tdeltaX: " + String(gX - oldGX));
-    Serial.println("\tdeltaY: " + String(gY - oldGY));
-    Serial.println("\tdeltaZ: " + String(gZ - oldGZ));
+      Serial.println("\tdeltaX: " + String(gX - oldGX));
+      Serial.println("\tdeltaY: " + String(gY - oldGY));
+      Serial.println("\tdeltaZ: " + String(gZ - oldGZ));
+      */
 
 
-    oldGX = gX;
-    oldGY = gY;
-    oldGZ = gZ;
-  
-    
+      oldGX = gX;
+      oldGY = gY;
+      oldGZ = gZ;
+
+      if (gX > maxX) {
+        maxX = gX;
+      }
+      if (gY > maxY) {
+        maxY = gY;
+      }
+      if (gZ > maxZ) {
+        maxZ = gZ;
+      }
+      if (gX < minX) {
+        minX = gX;
+      }
+      if (gY < minY) {
+        minY = gY;
+      }
+      if (gZ < minZ) {
+        minZ = gZ;
+      }
+      
+      
+      
+      Serial.println(String(gX) + "," + String(gY) + "," + String(gZ));
+      amaount++;
+    }
+  }
+  else {
+    if (canLoop) {
+      Serial.println("maxX: " + String(maxX) + ", minX: " + String(minX));
+      Serial.println("maxY: " + String(maxY) + ", minY: " + String(minY));
+      Serial.println("maxZ: " + String(maxZ) + ", minZ: " + String(minZ));
+      canLoop = false;
+    }
   }
   
 
@@ -80,11 +127,11 @@ void loop() {
 
   */
   
-  Serial.println("-----------------------------------");
+  //Serial.println("-----------------------------------");
   
 
   //Serial.print("\tApproxAltitude(m): ");
   //Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
-  delay(5000);
+  delay(10);
   //Serial.println(""); // Add an empty line. This text was added here from my tablet.
   }
