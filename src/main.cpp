970ddf7aedc6c93affee4d16ebfeeff2106e09b8
Andras <kaltenandras@gmail.com>
2026-02-17 20:57:59 +0200

    lora

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <MPU9250_asukiaaa.h>
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
+#include <HardwareSerial.h>
 
 
 Adafruit_BMP280 bme; // I2C
 MPU9250_asukiaaa mySensor;
+HardwareSerial LoRaSerial(1); //esp32-nek ez kell(elmeletileg)
 float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
 
 float offSetY = 3.44;
@@ -14,30 +16,85 @@ long lastTime;
 
 float alpha = 0.98;  // gyro trust factor
 
+#define TXD_PIN 2
+#define RXD_PIN 15
+
 float roll =0;
 float pitch = 0;
 float yaw = 0;
 
+void sendATCommand(String command) {
+  LoRaSerial.println(command); //\r\n\r\n\r\n"
+  delay(100);
+  String Message = "";
+  while (LoRaSerial.available()) {
+    Message += (char) LoRaSerial.read();
+  }
+  Serial.println(Message);
+}
+
+void sendBTCommand(String command) {
+  LoRaSerial.println(command); //\r\n\r\n\r\n"
+  Serial.println(command + "\r\n");
+  delay(2000);
+  String Message = "";
+  while (LoRaSerial.available()) {
+    Message += (char) LoRaSerial.read();
+  }
+  if (Message != ""){
+  Serial.println(Message);}
+  else {Serial.println("no response");}
+}
+
+void clearLoRaBuffer() {
+  while (LoRaSerial.available()) {
+    LoRaSerial.read();
+  }
+}
+
+void setupLoRa(){
+  //radio set sync
+  delay(1500);
+  clearLoRaBuffer();
+  delay(2000);
+  sendBTCommand("sys get ver");
+  sendBTCommand("sys reset");
+  delay(1000);
+  //sendBTCommand("mac pause");
+  //delay(1000);
+  sendBTCommand("radio set freq 863000000"); //868000000
+  sendBTCommand("radio set mod lora");
+  sendBTCommand("radio set pwr 20"); // POWER 20dbm (jobb lenne tobb ) akarmi lehet a foldi aallomason
+  sendBTCommand("radio set sync a1"); // sync word (same for reciver and transmiter!!!)
+  sendBTCommand("radio set bw 125"); // same for transmitter and reciver!!
+  sendBTCommand("radio set sf sf7"); // same for tx rx sf8
+}
+
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  bme.begin();
-  mySensor.beginAccel();
-  mySensor.beginGyro();
-  mySensor.beginMag();
+  Serial.begin(115200);
+  //Wire.begin();
+  //bme.begin();
+  //mySensor.beginAccel();
+  //mySensor.beginGyro();
+  //mySensor.beginMag();
 
   // You can set your own offset for mag values
   // mySensor.magXOffset = -50;
   // mySensor.magYOffset = -55;
   // mySensor.magZOffset = -10;
 
-
+  /*
   if (!bme.begin(0x76)) {
     Serial.println("BMP280 not found! Check wiring.");
     while (1);
   }
+  */
   lastTime = millis();
   //Serial.println("debug");
+  LoRaSerial.begin(115200, SERIAL_8N1, RXD_PIN, TXD_PIN);
+  //Serial.println(LoRaSerial.available());
+  //Serial.println("calling setupLoRa");
+  setupLoRa();
 }
 
 struct gyroscopeData
@@ -119,14 +176,15 @@ float readTMP117() {
 void loop() {
 
   //gyroscopeData gyroData = gyroscope();
-  float TMP117_data = readTMP117();
+  //float TMP117_data = readTMP117();
 
   //Serial.print("\tRoll: "); Serial.print(gyroData.roll, 2);
   //Serial.print("\tPitch: "); Serial.print(gyroData.pitch, 2);
   //Serial.print("\tYaw: "); Serial.println(gyroData.yaw, 2);
-  Serial.println(TMP117_data);
+  //Serial.println(TMP117_data);
   /*
   
+
   Serial.print("\tTemperature(*C): ");
   Serial.print(bme.readTemperature());
 
@@ -134,6 +192,10 @@ void loop() {
   Serial.print(bme.readPressure()/3377);
 
   */
+
+  sendBTCommand("radio tx 123 1");
+  Serial.println("sent");
+  delay(1000);
   
   //Serial.println("-----------------------------------");
 
